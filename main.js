353e4073fba0bfad21bfe1c5fc9b1c43394fd118
hunterjohnsondesigns <hunterjohnsongd@gmail.com>
2023-04-11 13:56:19 -0400

    mobile nav

--- main.js
+++ main.js
@@ -14,3 +14,17 @@ const addHandlers = () => {
   $(() => {
     addHandlers()
   })
+
+
+  window.onload=()=>{
+    const $ = document.querySelector.bind(document);
+const $All = document.querySelectorAll.bind(document);
+
+$('#menu').onclick=()=>{              $('#menu').classList.toggle('rotate')
+  $('.nav-page1').classList.toggle('transform')
+  $('.nav-page2').classList.toggle('transform')
+  $('.menu-line1').classList.toggle('rotate1')
+   $('.menu-line2').classList.toggle('rotate2')
+}
+}
+
